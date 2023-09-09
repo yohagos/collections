02ac6c337ef482a1a7a3d089580009b385a07287
yohagos <yosef.hagos@googlemail.com>
2023-09-09 15:27:18 +0200

    Introduction Collections Part VIII Sets Part 2

--- ProductCatalogue.java
+++ ProductCatalogue.java
@@ -11,4 +11,33 @@ public class ProductCatalogue implements  Iterable<Product> {
     public Iterator<Product> iterator() {
         return products.iterator();
     }
+
+    /*
+    * SortedSet
+    * - Defines an order
+    * - No indexes. but subset views possible
+    *
+    * E first();
+    * E last();
+    *
+    * SortedSet tailSet(E fromElement);
+    * SortedSet headSet(E fromElement);
+    * SortedSet subSet(E fromElement, E toElement);
+    * */
+
+    /*
+    * NavigableSet
+    * - Extends SortedSet
+    * - Provides ways to move through the order
+    * - Implemented by TreeSet
+    *
+    * E lower();
+    * E higher();
+    *
+    * E floor(E e);
+    * E ceiling(E e);
+    *
+    * E pollFirst(); --> returns Value, but removes it from Set
+    * E pollLast(); --> returns Value, but removes it from Set
+    * */
 }
